d06b15f39abe179ba5a3fadc2ff5e6a53ce6bdf2
Orien Longo <longo788@newschool.edu>
2015-11-16 10:45:46 -0500

    fixed build hopefullyaaAAAAAASHDSHHHHHG

--- Transparent/Assets/Scripts/Restart.cs
+++ Transparent/Assets/Scripts/Restart.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Restart : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Restart : MonoBehaviour
+{
+    public GameObject canvas1, canvas2;
+    // Use this for initialization
+    void Start()
+    {
         //Application.loadedLevel("LichHunt proto3");
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        if (Input.GetMouseButtonDown(0))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Joystick1Button0)|| Input.GetKeyDown(KeyCode.Joystick2Button0)|| Input.GetKeyDown(KeyCode.Joystick3Button0))
+        {
             Application.LoadLevel("LichHunt proto3");
-            Debug.Log("Pressed left click.");
-
+            //Debug.Log("Pressed left click.");
+            Destroy(canvas1);
+            Destroy(canvas1);
+        }
     }
 }
